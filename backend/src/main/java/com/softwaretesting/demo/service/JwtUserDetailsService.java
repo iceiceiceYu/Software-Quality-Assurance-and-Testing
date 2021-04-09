bac0cf23cb8c95b54daf04a4799ab3cafed8bc14
mikollin <aichaodieqin@126.com>
2021-04-09 19:50:09 +0800

    initial

---
+++ backend/src/main/java/com/softwaretesting/demo/service/JwtUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.softwaretesting.demo.service;
+
+import com.softwaretesting.demo.domain.Authority;
+import com.softwaretesting.demo.domain.User;
+import com.softwaretesting.demo.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author zyl
+ */
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+    private UserRepository userRepository;
+
+
+    public JwtUserDetailsService(UserRepository userRepository){
+                         this.userRepository=userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByName(username);
+        if (null == user) {
+            throw new UsernameNotFoundException("User: '" + username + "' not found.");
+        }
+        return new User(user.getUsername(), user.getPassword(), user.getAge(), user.getGender(),
+                (Set<Authority>) user.getAuthorities());
+    }
+}

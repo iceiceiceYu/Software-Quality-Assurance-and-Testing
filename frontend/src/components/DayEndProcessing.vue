d1d6d8fcb0aec83b428e63bbe58ca6f57378905d
taozixin <496935925@qq.com>
2021-04-10 23:26:54 +0800

    req1&2 update

--- frontend/src/components/DayEndProcessing.vue
+++ frontend/src/components/DayEndProcessing.vue
@@ -1,33 +1,44 @@
 <template>
-  <el-container>
-    <el-header>
-      <v-navbar :activeIndex="'1'"></v-navbar>
-    </el-header>
     <el-container>
-      <el-aside>
-        <v-side :activeIndex="'1-2'"></v-side>
-      </el-aside>
-      <el-main>
-        <h1>日终处理</h1>
-        <el-button>开始日终处理</el-button>
+        <el-header>
+            <v-navbar :activeIndex="'1'"></v-navbar>
+        </el-header>
+        <el-container>
+            <el-aside>
+                <v-side :activeIndex="'1-2'"></v-side>
+            </el-aside>
+            <el-main>
+                <h1>日终处理</h1>
+                <el-button @click="DayEndProcess">开始日终处理</el-button>
 
 
-      </el-main>
+            </el-main>
+        </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script>
-  export default {
-    name: "DayEndProcessing",
-    data() {
-      return {
-
-      };
-    },
-    methods: {
+    export default {
+        name: "DayEndProcessing",
+        data() {
+            return {};
+        },
+        methods: {
+            DayEndProcess() {
+                this.$axios.post('/repay/autoRepayment',
+                    true
+                ).then(resp => {
+                    this.$notify({
+                        title: '没有找到对应账户',
+                        type: 'success'
+                    });
+                }).catch(error => {
+                    console.log(error);
+                    alert('网络连接失败')
+                })
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,25 +6,25 @@ export default new Vuex.Store({
   state: {
     id: localStorage.getItem('id') || null,
     username: localStorage.getItem('username') || null,
-    type:localStorage.getItem('type')||null,
+
   },
   mutations: {
     login(state, data){
       localStorage.setItem('id', data.id)
       localStorage.setItem('username', data.username)
-      localStorage.setItem('type',data.type)
+
       state.id = data.id
       state.username = data.username
-      state.type =data.type
+
     },
     logout(state) {
       // 移除token
       localStorage.removeItem('id')
       localStorage.removeItem('username')
-      localStorage.removeItem('type')
+
       state.id = null
       state.username = null
-      state.type = null
+
     }
   },
   actions: {
